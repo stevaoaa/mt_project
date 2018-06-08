71b0f826b42935db6deffdd0e7d54d880ced8cf5
stevao-andrade <stevao.andrade@gmail.com>
2018-06-08 13:51:47 -0300

    initial commit to metamorphic_reations script

---
+++ metamorphic_relations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Will handle MR exections
+"""
+def MPublished(parameter_list):
+    raise NotImplementedError
+
+
+def MPTitle(parameter_list):
+    raise NotImplementedError
+
+
+
+def MPReverseJD_SwapJD(parameter_list):
+    raise NotImplementedError
+
+def Top1Absent(parameter_list):
+    raise NotImplementedError
