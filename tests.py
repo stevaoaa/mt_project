f9982b6a417c5d0b718a8c5738df03a30c7f2610
stevao-andrade <stevao.andrade@gmail.com>
2018-06-15 14:13:42 -0300

    convet tests.py in a simple python script without unitest

--- tests.py
+++ tests.py
@@ -7,115 +7,155 @@ import datetime
 
 import unittest
 
+#selenium
+from selenium import webdriver
+
 #local
 import util
 import engines
 import statistics
 
+
+from scripts.ACM_search import ACM
+from scripts.IEEE_search import IEEE
+from scripts.Springer_search import Springer
+from scripts.SCOPUS_search import Scopus
+from scripts.Scidirect_search import Scidirect
+
+
 """
     This class simple execute some functions in order to see how things are going until mix all things together
 """
-class SimplisticTest(unittest.TestCase):
+
+def test_random_line():
+
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
+
+    with open(path_file, "r") as afile:
+        line = util.random_line(afile)
+    
+    print(("A random line from a file: %s") % (line))
 
 
-    #util.py tests
-    def test_random_line(self):
+def test_random_keywords():
+    
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
 
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
-
-        with open(path_file, "r") as afile:
-            line = util.random_line(afile)
-        
-        print(("A random line from a file: %s") % (line))
-        self.assertTrue(True)
+    with open(path_file, "r") as afile:
+        keywords = util.random_keywords(afile)
+    
+    print(("A random keywords list from a file: %s") % (keywords))
 
 
+def test_file_in_list():
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
 
-    def test_random_keywords(self):
-        
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
-
-        with open(path_file, "r") as afile:
-            keywords = util.random_keywords(afile)
-        
-        print(("A random keywords list from a file: %s") % (keywords))
-        self.assertTrue(isinstance(keywords, list))
+    with open(path_file, "r") as afile:
+        alist = util.file_in_list(afile)
+    
+    print(("A list from a file: %s") % (alist[:2]))
 
 
+def test_calculate_elapsed_time():
+    
+    # for example
+    s1 = str(datetime.datetime.now().strftime('%H:%M:%S'))
+    time.sleep(1) #wait one second
+    s2 = str(datetime.datetime.now().strftime('%H:%M:%S'))
 
-    def test_file_in_list(self):
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
+    delta = util.calculate_elapsed_time(s1,s2) 
+    
+    #difference between 2 dates in '%H:%M:%S' format
+    print(("Difference between dates: %s") % (delta))
 
-        with open(path_file, "r") as afile:
-            alist = util.file_in_list(afile)
-        
-        print(("A list from a file: %s") % (alist[:2]))
-        self.assertTrue(isinstance(alist, list))
+    
+#statistics.py tests
+def test_check_similarity():
+    
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
 
+    with open(path_file, "r") as afile:
+        alist = util.file_in_list(afile)
 
-    def test_calculate_elapsed_time(self):
-        
-        # for example
-        s1 = str(datetime.datetime.now().strftime('%H:%M:%S'))
-        time.sleep(1) #wait one second
-        s2 = str(datetime.datetime.now().strftime('%H:%M:%S'))
-
-        delta = util.calculate_elapsed_time(s1,s2) 
-        
-        #difference between 2 dates in '%H:%M:%S' format
-        print(("Difference between dates: %s") % (delta))
-        self.assertTrue(True)
-
-        
-
-    #statistics.py tests
-    def test_check_similarity(self):
-        
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
-
-        with open(path_file, "r") as afile:
-            alist = util.file_in_list(afile)
-
-        #calculate jaccard wit a list and the same list reversed. Should be lower
-        jaccard = statistics.check_similarity(alist, alist[::-1])
-        print(("Jaccard Coefficient: %s") % (jaccard)) 
-        self.assertTrue(True)
+    #calculate jaccard wit a list and the same list reversed. Should be lower
+    jaccard = statistics.check_similarity(alist, alist[::-1])
+    print(("Jaccard Coefficient: %s") % (jaccard)) 
 
 
+def test_get_ROCOF():
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
+    with open(path_file, "r") as afile:
+        first_line = next(afile)
 
-    def test_get_ROCOF(self):
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
-        with open(path_file, "r") as afile:
-            first_line = next(afile)
-
-        #get the last element (thus, the time of execution)
-        last_element = first_line[-1]
-        print(last_element)
-
+    #get the last element (thus, the time of execution)
+    last_element = first_line[-1]
+    print(last_element)
 
 
-    def test_get_ROCOA(self):
-        pass
+def test_create_search_string():
+
+    path_file = os.getcwd() + "/dataset/keywords_sample.csv"
+
+    with open(path_file, "r") as afile:
+        keywords = util.random_keywords(afile)
+
+        result = engines.ieee_string(keywords,title="Some title of test")
+        print("IEEE|Springer String: %s" % (result))
+
+        result = engines.spring_string(keywords,title="Some title of test")
+        print("Springer String: %s" % (result))
+
+        result = engines.scopus_string(keywords,title="Some title of test")
+        print("Scopus String: %s" % (result))
+
+        result = engines.sciente_direct_string(keywords,title="Some title of test")
+        print("Science Direct String: %s" % (result))
+
+        result = engines.acm_string(keywords,title="Some title of test")
+        print("ACM String: %s" % (result))
 
 
-    def test_create_search_string(self):
+def test_run_a_query(engine, keywords, driver):
+    
+    #generate string
+    source_string = util.create_search_string(keywords, engine)
 
-        path_file = os.getcwd() + "/dataset/keywords_sample.csv"
+    #create the bots
+    acm_bot = ACM(source_string,driver)
+    ieee_bot = IEEE(source_string,driver)
+    scidirect_bot = Scidirect(source_string,driver)
+    scopus_bot = Scopus(source_string,driver)
+    springer_bot = Springer(source_string,driver)
 
-        with open(path_file, "r") as afile:
-            keywords = util.random_keywords(afile)
+    source_results = []
 
-            result = engines.ieee_spring_string(keywords,title="Some title of test")
-            print("IEEE|Springer String: %s" % (result))
+    #get the source query results
+    if engine == "ACM":
+        source_results = acm_bot.test_ACM()
 
-            result = engines.scopus_string(keywords,title="Some title of test")
-            print("Scopus String: %s" % (result))
+    if engine == "IEEE":
+        source_results = ieee_bot.test_IEEE()
 
-            result = engines.sciente_direct_string(keywords,title="Some title of test")
-            print("Science Direct String: %s" % (result))
+    if engine == "Scidirect":
+        source_results = scidirect_bot.test_scidirect()
 
-            result = engines.acm_string(keywords,title="Some title of test")
-            print("ACM String: %s" % (result))
+    if engine == "Springer":
+        source_results = springer_bot.test_springer()
+
+    if engine == "Scopus":
+        source_results = scopus_bot.test_Scopus()
+
+    print(source_results)
 
 if __name__ == '__main__':
-    unittest.main()
+    
+    #parameters
+    engine = "Springer"
+    keywords = ["Adaptive system", "Computer science", "Computer virus", "Electrical engineering"]
+    
+    #build driver path
+    base_dir = os.getcwd()
+    driver_path = base_dir + '/scripts/drivers/chromedriver'
+    driver = webdriver.Chrome(driver_path)
+
+    test_run_a_query(engine, keywords, driver)
